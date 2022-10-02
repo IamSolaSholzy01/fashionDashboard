64cc6b048b5b138238ac6c6245cb3a64d9dc0483
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-10-02 17:31:29 +0100

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { authStore } from "../stores/authStore";
 import Link from "next/link";
 import { userService } from "../services";
 
 const Home: NextPage<{}> = () => {
-  const { state, dispatch } = useContext(authStore);
+  const { state } = useContext(authStore);
   const user = userService.userValue;
   console.log(user);
   return (
